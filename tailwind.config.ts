df537c4cbfaf551005adfa91a3d9c5c09b436b51
Rohit A <rohit311710@gmail.com>
2024-02-18 20:53:09 +0530

    home page started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,22 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
+      colors: {
+        'prime': '#EB493C',
+        'br': '#02070D',
+      },
+      container: {
+        center: true,
+
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "globe":"url('../public/globe.jpg')",
+        "globe": "url('../public/globe.jpg')",
       },
     },
   },
