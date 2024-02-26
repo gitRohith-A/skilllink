0f50f0c6edb91fc37087da5433cf14fcbc0f0939
Rohit A <rohit311710@gmail.com>
2024-02-26 22:17:52 +0530

    add: home page correction made

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Banner from "@/components/Home/Banner";
+import Footer from "@/components/Home/Footer";
 import NavBar from "@/components/Home/NavBar";
+import SearchandFilters from "@/components/Home/SearchandFilters";
+import ServicesCard from "@/components/Home/ServicesCard";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <Banner />
-
+      <SearchandFilters />
+      <ServicesCard />
+      <Footer />
     </div>
   );
 }
