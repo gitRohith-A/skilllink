1311278eddebe546b411d792d2e27314c9f44913
Shuheb <mshuheb8@gmail.com>
2024-02-27 12:14:10 +0530

    Add: Tooltip and profile

--- components/dashboard/profile/ProfileFields.tsx
+++ components/dashboard/profile/ProfileFields.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { ProfileHeadProps } from './Profile'
+import { MdEdit } from "react-icons/md";
 
 const keyLabelMap: { [key: string]: string } = {
     email: 'Email',
@@ -10,7 +12,13 @@ const keyLabelMap: { [key: string]: string } = {
     occupation: 'Occupation',
 };
 
+export interface ModalType {
+    type: number,
+    id: number
+}
+
 const ProfileFields: React.FC<ProfileHeadProps> = ({ user }) => {
+    const [Modal, setModal] = useState<ModalType>({ type: 0, id: 0 })
 
     const renderUserInfo = () => {
         return Object.entries(keyLabelMap).map(([key, label]) => {
@@ -31,7 +39,7 @@ const ProfileFields: React.FC<ProfileHeadProps> = ({ user }) => {
             <div className="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
                 <div className="w-full flex flex-col 2xl:w-1/3">
                     <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
-                        <h4 className="text-xl text-gray-900 font-bold">Personal Info</h4>
+                        <h4 className="flex justify-between text-xl text-gray-900 font-bold">Personal Info <MdEdit size={15} className='text-orange-600' onClick={e => setModal({ type: 1, id: 1 })} /></h4>
                         <ul className="mt-2 text-gray-700">
                             {renderUserInfo()}
                         </ul>
@@ -39,8 +47,8 @@ const ProfileFields: React.FC<ProfileHeadProps> = ({ user }) => {
                 </div>
                 <div className="flex flex-col w-full 2xl:w-2/3">
                     <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
-                        <h4 className="text-xl text-gray-900 font-bold">About</h4>
-                        <p className="mt-2 text-gray-700">{user.about ? user.about : <span className='text-xs text-orange-500 underline'>Add About You</span>}</p>
+                        <h4 className="text-xl text-gray-900 font-bold flex ">About <MdEdit size={15} className='text-orange-600 ms-2 mt-1' /></h4>
+                        <p className="mt-2 text-gray-700 ">{user.about ? user.about : <span className='text-xs text-orange-500 underline'>Add About You</span>}</p>
                     </div>
                 </div>
             </div>
--- components/dashboard/profile/ProfileHead.tsx
+++ components/dashboard/profile/ProfileHead.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FaUserEdit } from "react-icons/fa";
+import { FaExclamation } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 import { ProfileHeadProps } from './Profile';
-
+import Tooltip from '@/components/others/tools/ToolTips';
 
 const ProfileHead: React.FC<ProfileHeadProps> = async ({ user }) => {
   return (
@@ -11,7 +12,7 @@ const ProfileHead: React.FC<ProfileHeadProps> = async ({ user }) => {
           <img src={user?.backgrounImage ? user?.backgrounImage : "https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg"} className="w-full h-full rounded-tl-lg rounded-tr-lg" />
         </div>
         <div className="flex flex-col items-center -mt-20">
-          <img src={user.image ?? ''} className="w-40 border-4 border-white rounded-full" />
+          <img src={user.image ?? user.image} className="w-40 border-4 border-white rounded-full" />
           <div className="flex items-center space-x-2 mt-2">
             <p className="text-2xl capitalize">{user.name}</p>
           </div>
@@ -19,14 +20,13 @@ const ProfileHead: React.FC<ProfileHeadProps> = async ({ user }) => {
           <p className="text-sm text-gray-500">{user.location}</p>
         </div>
         <div className="flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2">
+
           <div className="flex items-center space-x-4 mt-2">
             {user.boardingStatus === 1 ? (
-              <span className="text-red-500 underline">Complete your profile</span>
+              <Tooltip message='Compelete Profile'>
+                <span className="text-red-500 underline"> <FaExclamation /></span>
+              </Tooltip>
             ) : null}
-            <button className="flex items-center bg-orange-600 hover:bg-orange-700 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100">
-              <FaUserEdit />
-              <span>Edit</span>
-            </button>
           </div>
         </div>
       </div>
