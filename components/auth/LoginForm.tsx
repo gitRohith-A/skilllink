b4ac049fc1c251f7c75829ce6fac130f25d33bf1
Rohit A <rohit311710@gmail.com>
2024-03-09 19:52:53 +0530

    Add: login and register front-end done

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -7,7 +7,8 @@ import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { signIn } from 'next-auth/react';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
-
+import Image from 'next/image';
+import log from '../../public/home/log.svg'
 interface LoginFormProps {
 
 }
@@ -51,24 +52,27 @@ const LoginForm: React.FC<LoginFormProps> = () => {
 
     return (
         <section className="
-        bg-[#0C3350]
-         flex items-center  h-screen">
+        bg-slate-100
+         flex items-center  h-screen ">
             <div className="container ">
                 <div className="columns-2">
-                    <div className="mb-10 text-center md:mb-16 relative">
+                    <div className="mb-10 text-center md:mb-16 relative space-y-32">
 
                         <a href="/#" className="mx-auto inline-block max-w-[160px]">
                             <div className="flex justify-end my-4 absolute top-[-17.5px] left-[52.5%]
                                 ">
                                 <span className="relative flex h-3 w-3 ">
                                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
                                 </span>
                             </div>
-                            <h5 className="text-4xl font-bold text-white ">SkillLink</h5>
+                            <h5 className="text-4xl font-bold text-blue-600 ">SkillLink</h5>
                         </a>
+                        <div className='flex justify-center '>
+                            <Image src={log} alt='image' />
+                        </div>
                     </div>
-                    <div className="-mx-4  flex flex-wrap">
+                    <div className="mx-4  flex flex-wrap">
                         <div className="w-full">
 
                             <div className="relative mx-auto max-w-[525px] overflow-hidden rounded-3xl bg-white  px-10 py-16 text-center dark:bg-dark-2 sm:px-12 md:px-[60px]">
@@ -113,7 +117,7 @@ const LoginForm: React.FC<LoginFormProps> = () => {
                                     <div className="mb-5">
                                         <button
                                             type="submit"
-                                            className="w-full cursor-pointer rounded-xl border border-primary bg-primary px-5 py-3 text-base font-medium text-blue-700 transition hover:bg-opacity-90 hover:bg-blue-700 hover:delay-50  hover:text-white hover:ease-in-out duration-300 disabled:bg-slate-300 disabled:text-white"
+                                            className="w-full cursor-pointer rounded-xl border border-primary bg-primary px-5 py-3 text-base font-medium bg-blue-600 text-white hover:bg-blue-500"
                                             disabled={isPending}
                                         >
                                             Log In
