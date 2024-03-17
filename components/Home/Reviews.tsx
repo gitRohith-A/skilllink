dda4f9ad5f42280c71a6021eaaa430c9d133da6b
Rohit A <rohit311710@gmail.com>
2024-03-17 22:51:04 +0530

    Add: services section on home page

---
+++ components/Home/Reviews.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Reviews() {
+    return (
+        <div className='container py-3 px-12 '>
+            <h5 className='
+            text-zinc-700 text-xl font-bold leading-7
+            '>Reviews</h5>
+        </div>
+    )
+}
+
+export default Reviews
