ca8ef68456bda87e23716f83069476d826b0e21d
Rohit A <rohit311710@gmail.com>
2024-05-16 11:04:03 +0530

    conflict resolved

--- components/publicProfile/enterpriseList/BanEnterpriseList.tsx
+++ components/publicProfile/enterpriseList/BanEnterpriseList.tsx
@@ -1,17 +1,16 @@
-import Image from 'next/image'
 import React from 'react'
-import bannerimg from '../../public/home/bannerimg.svg'
-function Banner() {
+
+function BanEnterpriseList() {
     return (
-        <section className="w-full px-8 text-gray-700 py-[5rem] bg-no-repeat">
-            <div className="container grid justify-items-center">
+        <section className="w-full px-8 text-gray-700 py-[5rem] bg-blue-50">
+            <div className="container grid justify-items-center ">
                 <div className=" my-5 px-5 py-1 bg-prime size-fit rounded-[3rem]">
                     <p className='text-white '><small>
-                        Your Gateway to Business Success!
+                        Our Top Enterprise Listing
                     </small></p>
                 </div>
                 <h1 className='my-5 text-6xl font-bold'>
-                    Welcome to <span className='text-prime'>SkillLink</span>
+                    SkillLink <span className='text-prime'>Enterprise</span>
                 </h1>
                 <p className='my-5 '>
                     Connect with skilled professionals effortlessly
@@ -19,12 +18,12 @@ function Banner() {
                 <button className=" px-5 py-2 rounded-lg border border-prime border-opacity-50 text-prime hover:bg-prime hover:text-white" >
                     Get Started
                 </button>
-                <div className="div">
-                    <Image src={bannerimg} alt='image' />
-                </div>
+            </div>
+            <div className="div">
+                hello
             </div>
         </section>
     )
 }
 
-export default Banner
+export default BanEnterpriseList
