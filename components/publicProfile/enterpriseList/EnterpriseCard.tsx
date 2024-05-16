76968ae3804be7d49819f70d8c8bf1ca46f8c2ab
Rohit A <rohit311710@gmail.com>
2024-05-16 12:25:54 +0530

    enterprise listing cards done

---
+++ components/publicProfile/enterpriseList/EnterpriseCard.tsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import { FaCircleUser } from "react-icons/fa6";
+
+function EnterpriseCard() {
+    return (
+        <div className="flex flex-wrap p-16 px-32 py-12 justify-between bg-gray-50">
+            <div className="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
+                <div
+                    className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                    <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-blue-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                    <div className="relative z-10 mx-auto max-w-md">
+                        <span className="grid h-20 w-20 place-items-center rounded-full bg-blue-500 transition-all duration-300 group-hover:bg-blue-400">
+                            <FaCircleUser fill='white' size={50} />
+                        </span>
+                        <div
+                            className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share
+                                online.
+                            </p>
+                        </div>
+                        <div className="pt-5 text-base font-semibold leading-7">
+                            <p>
+                                <a href="#" className="text-blue-500 transition-all duration-300 group-hover:text-white">
+                                    View Profile
+                                    &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
+                <div
+                    className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                    <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-blue-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                    <div className="relative z-10 mx-auto max-w-md">
+                        <span className="grid h-20 w-20 place-items-center rounded-full bg-blue-500 transition-all duration-300 group-hover:bg-blue-400">
+                            <FaCircleUser fill='white' size={50} />
+                        </span>
+                        <div
+                            className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share
+                                online.
+                            </p>
+                        </div>
+                        <div className="pt-5 text-base font-semibold leading-7">
+                            <p>
+                                <a href="#" className="text-blue-500 transition-all duration-300 group-hover:text-white">
+                                    View Profile
+                                    &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
+                <div
+                    className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                    <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-blue-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                    <div className="relative z-10 mx-auto max-w-md">
+                        <span className="grid h-20 w-20 place-items-center rounded-full bg-blue-500 transition-all duration-300 group-hover:bg-blue-400">
+                            <FaCircleUser fill='white' size={50} />
+                        </span>
+                        <div
+                            className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share
+                                online.
+                            </p>
+                        </div>
+                        <div className="pt-5 text-base font-semibold leading-7">
+                            <p>
+                                <a href="#" className="text-blue-500 transition-all duration-300 group-hover:text-white">
+                                    View Profile
+                                    &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
+                <div
+                    className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                    <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-blue-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                    <div className="relative z-10 mx-auto max-w-md">
+                        <span className="grid h-20 w-20 place-items-center rounded-full bg-blue-500 transition-all duration-300 group-hover:bg-blue-400">
+                            <FaCircleUser fill='white' size={50} />
+                        </span>
+                        <div
+                            className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+                            <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share
+                                online.
+                            </p>
+                        </div>
+                        <div className="pt-5 text-base font-semibold leading-7">
+                            <p>
+                                <a href="#" className="text-blue-500 transition-all duration-300 group-hover:text-white">
+                                    View Profile
+                                    &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default EnterpriseCard
